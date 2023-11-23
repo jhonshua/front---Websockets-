f22823890fa7ddaedacfc5cc4d5767f09d19e88e
Julio Cesar Llinas <109869139+jhonshua@users.noreply.github.com>
2023-11-23 12:38:49 -0400

    Update App.jsx
    change adress api

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import {UserContextProvider} from "./UserContext";
 import Routes from "./Routes";
 
 function App() {
-  axios.defaults.baseURL = 'https://apiwebsockets-dev-ccgr/';
+  axios.defaults.baseURL = 'https://apiwebsockets-dev-ccgr.3.us-1.fl0.io/';
   axios.defaults.withCredentials = true;
   return (
     <UserContextProvider>
